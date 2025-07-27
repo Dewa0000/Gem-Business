3165165e77cf390600440e023278d86d3cdb0c63
Dewa0000 <Dewashishsahu8888@gmail.com>
2025-07-27 17:59:34 +0530

    6th

--- gem-business-backend/Models/authModel.js
+++ gem-business-backend/Models/authModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const bcryptjs = require("bcryptjs");
-const validator = require("validator");
 const schema = mongoose.Schema;
+const validator = require("validator");
+const bcryptjs = require("bcryptjs");
 
 const authSchema = new schema({
     userName:{
@@ -18,10 +18,35 @@ const authSchema = new schema({
         required:true
     },
     moNumber: {
-        type:Number,
+        type:String,
         required:true,
     }
 })
+
+authSchema.statics.signup = async function(userName,email,password,moNumber){
+    if(!userName || !email || !password || !moNumber){
+        throw Error("All fields are required");
+    }
+    if(!validator.isEmail(email)){
+        throw Error("Invalid email");
+    }
+    if(!validator.isStrongPassword(password)){
+        throw Error("Password is not strong enough");
+    }
+    const exists = await this.findOne({email});
+    if(exists){
+        throw Error("Email already in use");
+    }
+    const salt = await bcryptjs.genSalt(10);
+    const hash = await bcryptjs.hash(password,salt);
+    const user = await this.create({
+        userName,
+        email,
+        moNumber,
+        password:hash
+    });    
+    return user;
+}   
 
 const Auth = mongoose.model("Auth", authSchema);
 module.exports = Auth;
--- gem-business-backend/Routes/authRoutes.js
+++ gem-business-backend/Routes/authRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const signup = require("../Controllers/authController.js")
 
-router.post("/auth/signup",signup)
+router.post("auth/signup",signup)
 
 module.exports = router;
