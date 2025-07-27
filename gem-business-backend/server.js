d85f76d4fea0cc402cd173e1fe9f1e797aeea351
Dewa0000 <Dewashishsahu8888@gmail.com>
2025-07-27 15:38:40 +0530

    4th

--- gem-business-backend/server.js
+++ gem-business-backend/server.js
@@ -18,6 +18,10 @@ mongoose.connect(process.env.MONGO_URL, {
     console.log(err);
 });
 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
