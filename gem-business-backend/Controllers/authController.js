1e350ad17dec0b10f3168b3e69bfe93ce83d7d3b
Dewa0000 <Dewashishsahu8888@gmail.com>
2025-07-27 19:13:56 +0530

    9th

--- gem-business-backend/Controllers/authController.js
+++ gem-business-backend/Controllers/authController.js
@@ -2,8 +2,13 @@ const Auth = require("../Models/authModel.js");
 
 const signup = async (req, res) => {
     const {userName,email,password,moNumber} =req.body;
-    console.log(userName,email,password,moNumber);
-    res.send("username:", userName)
+    
+    try{
+        const user = await Auth.signup(userName,email,password,moNumber);
+        res.status(201).json(user)
+    }catch(err){
+        res.status(400).json({error: err.message})
+    }
 
 }
 
