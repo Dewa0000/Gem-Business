54d02625b354f6171f479edb4627f1947ad2b314
Dewa0000 <Dewashishsahu8888@gmail.com>
2025-08-04 22:50:36 +0530

    12th

--- gem-business-backend/Routes/authRoutes.js
+++ gem-business-backend/Routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const signup = require("../Controllers/authController.js")
+const login = require("../Controllers/authController.js")
 
 router.post("/auth/signup",signup)
 router.post("/auth/login",login)
