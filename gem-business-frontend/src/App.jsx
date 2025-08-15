6d6902f94d640fa8a930fb6bc79fc00aab129803
Dewa0000 <Dewashishsahu8888@gmail.com>
2025-08-15 14:14:59 +0530

    48th

--- gem-business-frontend/src/App.jsx
+++ gem-business-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
 import SignUpForm from './Pages/signUpForm'
 import './App.css'
 import LoginForm from './Pages/loginForm'
+import Header from './Components/Header'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
   return (
      <>
      <Router>
+      <Header/>
       <Routes>
         <Route path='/signup' element= {<SignUpForm/>}/>
         <Route path='/login' element = {<LoginForm/>}/>
