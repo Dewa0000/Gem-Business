aaa364ab349bc7644ed45c558ce5c6a8560b34b9
Dewa0000 <Dewashishsahu8888@gmail.com>
2025-08-15 21:27:35 +0530

    63rd

--- gem-business-frontend/src/Pages/productsPage.jsx
+++ gem-business-frontend/src/Pages/productsPage.jsx
@@ -19,41 +19,7 @@ const ProductsPage = () => {
               </p>
             </div>
 
-            {/* Filter Buttons */}
-            <div className="flex gap-3 p-3 flex-wrap pr-4">
-              <button className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-lg bg-[#1b3124] pl-4 pr-2">
-                <p className="text-white text-sm font-medium leading-normal">Type</p>
-                <div className="text-white" data-icon="CaretDown" data-size="20px" data-weight="regular">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
-                    <path d="M213.66,101.66l-80,80a8,8,0,0,1-11.32,0l-80-80A8,8,0,0,1,53.66,90.34L128,164.69l74.34-74.35a8,8,0,0,1,11.32,11.32Z"></path>
-                  </svg>
-                </div>
-              </button>
-              <button className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-lg bg-[#1b3124] pl-4 pr-2">
-                <p className="text-white text-sm font-medium leading-normal">Price</p>
-                <div className="text-white" data-icon="CaretDown" data-size="20px" data-weight="regular">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
-                    <path d="M213.66,101.66l-80,80a8,8,0,0,1-11.32,0l-80-80A8,8,0,0,1,53.66,90.34L128,164.69l74.34-74.35a8,8,0,0,1,11.32,11.32Z"></path>
-                  </svg>
-                </div>
-              </button>
-              <button className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-lg bg-[#1b3124] pl-4 pr-2">
-                <p className="text-white text-sm font-medium leading-normal">Material</p>
-                <div className="text-white" data-icon="CaretDown" data-size="20px" data-weight="regular">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
-                    <path d="M213.66,101.66l-80,80a8,8,0,0,1-11.32,0l-80-80A8,8,0,0,1,53.66,90.34L128,164.69l74.34-74.35a8,8,0,0,1,11.32,11.32Z"></path>
-                  </svg>
-                </div>
-              </button>
-              <button className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-lg bg-[#1b3124] pl-4 pr-2">
-                <p className="text-white text-sm font-medium leading-normal">More Filters</p>
-                <div className="text-white" data-icon="CaretDown" data-size="20px" data-weight="regular">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
-                    <path d="M213.66,101.66l-80,80a8,8,0,0,1-11.32,0l-80-80A8,8,0,0,1,53.66,90.34L128,164.69l74.34-74.35a8,8,0,0,1,11.32,11.32Z"></path>
-                  </svg>
-                </div>
-              </button>
-            </div>
+            
 
             {/* Product Grid */}
             <div className="grid grid-cols-[repeat(auto-fit,minmax(158px,1fr))] gap-3 p-4">
@@ -74,36 +40,6 @@ const ProductsPage = () => {
                     </div>
                   </div>
                 ))}
-            </div>
-
-            {/* Pagination */}
-            <div className="flex items-center justify-center p-4">
-              <a href="#" className="flex size-10 items-center justify-center">
-                <div className="text-white" data-icon="CaretLeft" data-size="18px" data-weight="regular">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="currentColor" viewBox="0 0 256 256">
-                    <path d="M165.66,202.34a8,8,0,0,1-11.32,11.32l-80-80a8,8,0,0,1,0-11.32l80-80a8,8,0,0,1,11.32,11.32L91.31,128Z"></path>
-                  </svg>
-                </div>
-              </a>
-              <a className="text-sm font-bold leading-normal tracking-[0.015em] flex size-10 items-center justify-center text-white rounded-full bg-[#1b3124]" href="#">
-                1
-              </a>
-              <a className="text-sm font-normal leading-normal flex size-10 items-center justify-center text-white rounded-full" href="#">
-                2
-              </a>
-              <a className="text-sm font-normal leading-normal flex size-10 items-center justify-center text-white rounded-full" href="#">
-                3
-              </a>
-              <a className="text-sm font-normal leading-normal flex size-10 items-center justify-center text-white rounded-full" href="#">
-                4
-              </a>
-              <a href="#" className="flex size-10 items-center justify-center">
-                <div className="text-white" data-icon="CaretRight" data-size="18px" data-weight="regular">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="currentColor" viewBox="0 0 256 256">
-                    <path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path>
-                  </svg>
-                </div>
-              </a>
             </div>
           </div>
         </div>
