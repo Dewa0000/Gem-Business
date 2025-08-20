d66dd264c0d9201ed3ea69b035425ebd209c9533
Dewa0000 <Dewashishsahu8888@gmail.com>
2025-08-20 16:25:30 +0530

    117th

--- gem-business-frontend/src/Pages/CartPage.jsx
+++ gem-business-frontend/src/Pages/CartPage.jsx
@@ -3,7 +3,7 @@ import { useCart } from "../Context/cartContext";
 
 const CartReviewPage = () => {
   const { cart, setCart } = useCart();
-  console.log(cart);
+  const total = cart.reduce((acc,item) => {return acc + item.price*item.quantity,0})
   return (
     <div
       className="relative flex size-full min-h-screen flex-col bg-[#122118] group/design-root overflow-x-hidden"
@@ -32,25 +32,14 @@ const CartReviewPage = () => {
                       <div  className="flex items-center text-white text-base font-medium gap-4 bg-[#1b3124] px-4 min-h-[72px] py-2 justify-between">
                         {item.name}</div>
                       <div  className="flex items-center text-white text-base font-medium gap-4 bg-[#1b3124] px-4 min-h-[72px] py-2 justify-between">
-                        {item.price} x {item.quantity}</div>
+                      ₹{item.price} x {item.quantity}</div>
                         <button onClick={() => removeFromCart()} className="text-red-500 text-sm font-medium hover:text-red-700">Remove</button>
                     </article>)
                   })}
                 </div>
               </div>
             ) : (<div className="text-[#6a7881] text-sm font-normal leading-normal p-4">Your Cart is empty</div>)}
-            <div className="flex items-center gap-4 bg-[#1b3124] px-4 min-h-[72px] py-2 justify-between">
-              <div className="flex items-center gap-4">
-
-                <div className="flex flex-col justify-center">
-                  <p className="text-white text-base font-medium leading-normal line-clamp-1">S</p>
-                  <p className="text-[#96c5a8] text-sm font-normal leading-normal line-clamp-2">Quantity: 1</p>
-                </div>
-              </div>
-              <div className="shrink-0">
-                <p className="text-white text-base font-normal leading-normal">$250</p>
-              </div>
-            </div>
+            
 
             {/* Order Summary */}
             <h3 className="text-white text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">
@@ -58,8 +47,8 @@ const CartReviewPage = () => {
             </h3>
             <div className="p-4 bg-[#1b3124]">
               <div className="flex justify-between gap-x-6 py-2">
-                <p className="text-[#96c5a8] text-sm font-normal leading-normal">Subtotal</p>
-                <p className="text-white text-sm font-normal leading-normal text-right">$1000</p>
+                <p className="text-[#96c5a8] text-sm font-normal leading-normal">total</p>
+                <p className="text-white text-sm font-normal leading-normal text-right">₹{total}</p>
               </div>
               <div className="flex justify-between gap-x-6 py-2">
                 <p className="text-[#96c5a8] text-sm font-normal leading-normal">Shipping</p>
@@ -67,7 +56,7 @@ const CartReviewPage = () => {
               </div>
               <div className="flex justify-between gap-x-6 py-2">
                 <p className="text-[#96c5a8] text-sm font-normal leading-normal">Taxes</p>
-                <p className="text-white text-sm font-normal leading-normal text-right">$50</p>
+                <p className="text-white text-sm font-normal leading-normal text-right">₹50</p>
               </div>
               <div className="flex justify-between gap-x-6 py-2">
                 <p className="text-[#96c5a8] text-sm font-normal leading-normal">Total</p>
