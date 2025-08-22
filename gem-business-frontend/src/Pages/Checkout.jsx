46584bdee084574bb1131196c01b0a1cb3ba7874
Dewa0000 <Dewashishsahu8888@gmail.com>
2025-08-22 22:00:15 +0530

    134th

--- gem-business-frontend/src/Pages/Checkout.jsx
+++ gem-business-frontend/src/Pages/Checkout.jsx
@@ -286,7 +286,7 @@ const CheckoutPage = () => {
               <h3 className="text-white text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">
                 Order Summary
               </h3>
-              <div className="flex items-center gap-4 bg-[#1b3124] px-4 min-h-[72px] py-2">
+              <div className="flex items-center justify-center gap-4 bg-[#1b3124] px-4 min-h-[72px] py-2">
                 {cart && cart.map((item,index) => {
                    return (<div key={index}
                    className="bg-center bg-no-repeat aspect-square bg-cover rounded-lg size-14"
