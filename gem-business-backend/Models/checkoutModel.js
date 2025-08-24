b66bbf4976222b38b2b7990b7f81eb7984161b0d
Dewa0000 <Dewashishsahu8888@gmail.com>
2025-08-24 14:47:34 +0530

    139th

--- gem-business-backend/Models/checkoutModel.js
+++ gem-business-backend/Models/checkoutModel.js
@@ -1,31 +1,31 @@
-const mongoose = require("mongoose");
-
 const checkoutSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Auth",
     required: true,
   },
-  orderItems: {
-    name: { type: String, required: true },
-    quantity: { type: Number, required: true },
-    image: { type: String, required: true },
-    price: { type: Number, required: true },
-    product: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Product",
-      required: true,
+  orderItems: [
+    {
+      name: { type: String, required: true },
+      quantity: { type: Number, required: true },
+      image: { type: String, required: true },
+      price: { type: Number, required: true },
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
     },
-  },
+  ],
   fullName: { type: String, required: true },
   email: { type: String, required: true },
   number: { type: String, required: true },
   address: { type: String, required: true },
   city: { type: String },
   pincode: { type: Number },
-  country: { type: String, required: true }, // Updated to required
-  shippingMethod: { type: String, required: true }, // Changed from 'shipping' to 'shippingMethod'
-  paymentMethod: { type: String, required: true }, // Changed from 'payment' to 'paymentMethod'
+  country: { type: String, required: true },
+  shippingMethod: { type: String, required: true },
+  paymentMethod: { type: String, required: true },
   totalPrice: { type: Number, required: true },
 });
 
