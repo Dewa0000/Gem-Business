98cd3c68220aaa519cc1806abd144ea048030863
Dewa0000 <Dewashishsahu8888@gmail.com>
2025-10-06 21:20:16 +0530

    224th

--- gem-business-backend/Routes/productRoutes.js
+++ gem-business-backend/Routes/productRoutes.js
@@ -11,14 +11,19 @@ router.get("/" , async (req,res) => {
     }
 })
 
-router.get("/:id", async (req,res) => {
-    try{
-        const product = await Product.findById(req.params.id);
-        if(!product) return console.log("No Product Found")
-            res.status(400).json(product);
-    }catch(err){
-        res.status(200).json({error: err.message})
+router.get("/:id", async (req, res) => {
+    try {
+      const product = await Product.findById(req.params.id);
+  
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+  
+      res.status(200).json(product);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
     }
-})
+  });
+  
 
 module.exports = router;
