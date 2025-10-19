89cd62b0eab8c7d88a2c75308c4394bad5efa800
Dewa0000 <Dewashishsahu8888@gmail.com>
2025-10-19 20:36:11 +0530

    236th

--- gem-business-backend/Controllers/blogController.js
+++ gem-business-backend/Controllers/blogController.js
@@ -14,4 +14,15 @@ const getBlogs = async (req, res) => {
     }
 }
 
-module.exports = {getBlogs};
+const getBlogById = async (req, res) => {
+    const {id} = req.params.id;
+    try{
+        const blog = await Blog.findById(id);
+        if(!blog) return res.status(404).json({error: "Blog not found"});
+        res.status(200).json({blog});
+    }catch(err){
+        res.status(500).json({error:"Failed to fetch blog"})
+    }
+}
+
+module.exports = {getBlogs,getBlogById};
--- gem-business-backend/Routes/blogRoutes.js
+++ gem-business-backend/Routes/blogRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {getBlogs} = require("../Controllers/blogController");
+const {getBlogs,getBlogById} = require("../Controllers/blogController");
 
 router.get("/", getBlogs);
+router.get("/:id", getBlogById )
 
 module.exports = router;
